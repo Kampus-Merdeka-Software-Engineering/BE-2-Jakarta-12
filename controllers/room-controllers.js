9e57bfe3998c3a8960ac2bb6795563d4c73c7990
Mnan <you@example.com>
2023-12-07 08:59:41 +0700

    lanjut lagi

--- controllers/room-controllers.js
+++ controllers/room-controllers.js
@@ -6,7 +6,7 @@ export const getRoomList = async(req, res) => {
     res.status(200).json({
         data: roomList.map((data) => ({
             ...data.dataValues,
-            room_image: `http://localhost:4000/${data.dataValues.room_image}`
+            room_image: `https://be-2-jakarta-12-production.up.railway.app/${data.dataValues.room_image}`
         })),
         message: "sukses mengambil data"
     })
